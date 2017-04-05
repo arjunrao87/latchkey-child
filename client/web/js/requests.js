b827027f372a87fec75d56797802790d92a94622
arjunrao87 <sporty.arjun@gmail.com>
2017-04-04 21:21:09 -0400

    Fleshing out web client

--- client/web/js/requests.js
+++ client/web/js/requests.js
@@ -1,7 +1,7 @@
 $( "#unlockButton" ).click(function() {
   var code = $('#code').val();
   console.log( "Sending unlock post request with " + code );
-  $.post( "http://localhost:3000/unlock", { code: "Xyzzy" })
+  $.post( "http://localhost:3000/unlock", { code: code })
   .done(function( data ) {
     console.log( "Received response from server = " + data );
   });
